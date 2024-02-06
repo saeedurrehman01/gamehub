18f00e4a0406cd4d6c1e6008fb0d3bce88b2ba91
saeedurrehman01 <139468834+saeedurrehman01@users.noreply.github.com>
2024-02-06 14:51:14 +0500

    Created a generic data hook

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -2,14 +2,14 @@
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { genres } = useGenres();
-  return (
-    <ul>
-      {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
-      ))}
-    </ul>
-  );
+  const { data } = useGenres();
+    return (
+        <ul>
+            {data.map((genre) => (
+                <li key={genre.id}>{genre.name}</li>
+            ))}
+        </ul>
+    );
 };
 
 export default GenreList;
