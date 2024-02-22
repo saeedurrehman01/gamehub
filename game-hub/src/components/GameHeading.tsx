6f1891e01fe5e6f9c2ffe0f6b8163e32a70e4667
saeedurrehman01 <139468834+saeedurrehman01@users.noreply.github.com>
2024-02-22 11:58:07 +0500

    Added Emojis

--- game-hub/src/components/GameHeading.tsx
+++ game-hub/src/components/GameHeading.tsx
@@ -9,8 +9,8 @@ const GameHeading = ({ gameQuery }: Props) => {
   const heading = `${gameQuery.platform?.name || ""} ${
     gameQuery.genre?.name || ""
   } Games`;
-    return (
-        <Heading as="h1" marginY={5} fontSize='5xl'>
+  return (
+    <Heading as="h1" marginY={5} fontSize="5xl">
       {heading}
     </Heading>
   );
